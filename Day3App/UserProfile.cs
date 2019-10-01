b96b1f6cf6514166e48995d9d37e99dc5b43b7de
vicpa34@yahoo.com <vicpa34@yahoo.com>
2019-10-01 18:49:51 +0300

    With calculate age function

---
+++ Day3App/UserProfile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3App
+{
+    class UserProfile
+    {
+        string FullName;
+        enum Gender
+        {
+            m,
+            f
+        }
+        DateTime birthDate;
+
+        public static int GetAge(DateTime birthDate)
+        {
+            DateTime n = DateTime.Now; // To avoid a race condition around midnight
+            int age = n.Year - birthDate.Year;
+
+            if (n.Month < birthDate.Month || (n.Month == birthDate.Month && n.Day < birthDate.Day))
+                age--;
+
+            return age;
+        }
+         
+
+        
+    }
+}
