7214b97772d347f30ccec38152627e7a4276e827
vicpa34@yahoo.com <vicpa34@yahoo.com>
2019-10-01 16:52:54 +0300

    added exception and processing logics

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,7 +10,43 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int num1 = GetNumber("Enter number A: ");
+                int num2 = GetNumber("Enter number B: ");
 
+
+                Console.WriteLine("Division = {0}",
+                Calculator.Divide(num1, num2));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Neparedzeta kluda");
+            }
+
+
+            Console.Read();
+        }
+        static int GetNumber(string text)
+        {
+            
+            Console.Write(text);
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+
+            try
+            {
+                int result = int.Parse(input);
+                return result;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+                GetNumber(text);  //recursion - call out function where we are
+            }
         }
     }
 }
